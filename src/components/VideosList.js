5cae4d4a6cdf1f3167962c43f7bf05713f0642c6
annafractuous <anna.rasshivkina@flatironschool.com>
2017-02-27 21:06:14 -0500

    make VideosList a stateless functional component

--- src/components/VideosList.js
+++ src/components/VideosList.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
 import VideoItem from './VideoItem';
 
-class VideosList extends Component {
-    render() {
-        let videos = this.props.videosData ?
-            this.props.videosData.map((video, idx) => {
-                return <VideoItem
-                    key={idx}
-                    idx={idx}
-                    video={video}
-                    onVideoSelect={this.props.onVideoSelect}
-                />
-            })
-            : (<p>Channel loading...</p>);
-        return (
-            <section className='container'>
-                <div className='videos-list col-xs-12 col-md-8 col-md-offset-2'>
-                    {videos}
-                </div>
-            </section>
-        )
-    }
-}
-
-VideosList.PropTypes = {
-    videosData: React.PropTypes.object.required,
-    onVideoSelect: React.PropTypes.func.required
+const VideosList = ({videosData, onVideoSelect}) => {
+    let videos = videosData ?
+        videosData.map((video, idx) => {
+            return <VideoItem
+                key={idx}
+                idx={idx}
+                video={video}
+                onVideoSelect={onVideoSelect}
+            />
+        })
+        : (<p>Channel loading...</p>);
+    return (
+        <section className='container'>
+            <div className='videos-list col-xs-12 col-md-8 col-md-offset-2'>
+                {videos}
+            </div>
+        </section>
+    )
 }
 
 export default VideosList;
