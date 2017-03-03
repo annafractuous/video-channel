8c2d56aaafd0041511ba6c6f7e4f3fb124109b35
annafractuous <anna.rasshivkina@flatironschool.com>
2017-03-02 22:35:23 -0500

    change 'shallow' to 'mount' in VideoItem tests

--- src/test/VideoItem.spec.js
+++ src/test/VideoItem.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, debug } from 'enzyme';
+import { mount, debug } from 'enzyme';
 import chai, { expect } from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 
@@ -27,7 +27,7 @@ const video = {
 
 describe('<VideoItem>', function () {
     it('should display video thumbnail & duration', function () {
-        const wrapper = shallow(<VideoItem video={video}/>);
+        const wrapper = mount(<VideoItem video={video}/>);
         let thumbnail = wrapper.find('.video-item-thumbnail img'),
             duration = wrapper.find('.video-item-thumbnail__video-length');
         expect(thumbnail).to.have.attr('src', video.thumbnail_medium);
@@ -35,7 +35,7 @@ describe('<VideoItem>', function () {
     });
 
     it('should display a title, description and user', function () {
-        const wrapper = shallow(<VideoItem video={video}/>);
+        const wrapper = mount(<VideoItem video={video}/>);
         let title = wrapper.find('.video-item-details__description-title'),
             description = wrapper.find('.video-item-details__description-summary'),
             user = wrapper.find('.video-item-details__info-user');
@@ -45,28 +45,28 @@ describe('<VideoItem>', function () {
     });
 
     it('should cut off long descriptions with an ellipsis', function () {
-        const wrapper = shallow(<VideoItem video={video}/>);
-        let description = wrapper.find('.video-item-details__description-summary').text();
-        let lastIdx = description.length - 1;
+        const wrapper = mount(<VideoItem video={video}/>);
+        let description = wrapper.find('.video-item-details__description-summary').text(),
+            lastIdx = description.length - 1;
         expect(description.slice(lastIdx - 3, lastIdx)).to.equal('...');
     });
 
     it('should say there is no description if the description is blank', function () {
         let noDescVideo = Object.assign({}, video, { description: '' });
-        const wrapper = shallow(<VideoItem video={noDescVideo}/>);
+        const wrapper = mount(<VideoItem video={noDescVideo}/>);
         let description = wrapper.find('.video-item-details__description-summary');
         expect(description).to.contain.text('There is no description for this video.');
     });
 
     it('should shorten numbers over 1000 to _K', function () {
-        const wrapper = shallow(<VideoItem video={video}/>);
+        const wrapper = mount(<VideoItem video={video}/>);
         let numPlays = wrapper.find('.video-item-details__info-stats__stat-plays');
         expect(numPlays).to.contain.text('71K');
     });
 
     it('should put 0 if a stat is blank e.g. undefined', function () {
         let noStatVideo = Object.assign({}, video, { stats_number_of_comments: undefined });
-        const wrapper = shallow(<VideoItem video={noStatVideo}/>);
+        const wrapper = mount(<VideoItem video={noStatVideo}/>);
         let numComments = wrapper.find('.video-item-details__info-stats__stat-comments');
         expect(numComments).to.contain.text('0');
     });
